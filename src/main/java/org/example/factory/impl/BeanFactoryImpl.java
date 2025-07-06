dfed2dc4f1e64e9d1e75329cbc361bd49e2356a2
Gleb <sorokinga04@mail.ru>
2025-07-06 14:29:45 +0400

    OR-2: Исправил ошибку

--- src/main/java/org/example/factory/impl/BeanFactoryImpl.java
+++ src/main/java/org/example/factory/impl/BeanFactoryImpl.java
@@ -8,11 +8,10 @@ import java.lang.reflect.InvocationTargetException;
 public class BeanFactoryImpl implements BeanFactory {
     @Override
     public <T> T createObject(Class<T> tClass) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-            boolean annotationPresent = tClass.isAnnotationPresent(Component.class);
-            if (annotationPresent) {
-                return tClass.getDeclaredConstructor().newInstance();
-            }
-            return null;
+        boolean annotationPresent = tClass.isAnnotationPresent(Component.class);
+        if (annotationPresent) {
+            return tClass.getDeclaredConstructor().newInstance();
         }
+        return null;
     }
 }
