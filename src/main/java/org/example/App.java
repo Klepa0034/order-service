77f7422e1be1b46f38b78a5990db993c28e2578c
Klepa0034 <sorokinga04@mail.ru>
2025-07-08 23:03:11 +0400

    OR-3: Написал DI( подготовка перед разработкой ) (#13)
    OR-3: Написал DI

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,20 @@
 package org.example;
 
-public class App 
+import org.example.entity.Dog;
+import org.example.entity.Employee;
+import org.example.factory.BeanProcessFactory;
+import org.example.factory.impl.BeanProcessImpl;
+import org.reflections.Reflections;
+
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
     }
 }
