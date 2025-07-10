429b01198eb6d117c773f3b4e3f26920a8e3d873
Gleb <sorokinga04@mail.ru>
2025-07-10 11:35:54 +0400

    OR-5: Исправил коменты

--- src/main/java/org/example/array/CustomArray.java
+++ src/main/java/org/example/array/CustomArray.java
@@ -4,7 +4,10 @@ public interface CustomArray<T> {
     void addElement(T value);
 
     void deleteElement(int index);
+
     void print();
-     T getElement(int index);
+
+    T getElement(int index);
+
     void updateElement(int index, T value);
 }
--- src/main/java/org/example/array/impl/CustomArrayImpl.java
+++ src/main/java/org/example/array/impl/CustomArrayImpl.java
@@ -1,23 +1,28 @@
 package org.example.array.impl;
 
 import org.example.array.CustomArray;
+
 public class CustomArrayImpl<T> implements CustomArray<T> {
     private int currentSize;
     private Object[] array;
+
     @Override
     public T getElement(int index) {
         checkIndex(index);
         return (T) array[index];
     }
+
     public CustomArrayImpl() {
         this.currentSize = 0;
         this.array = new Object[10];
     }
+
     @Override
     public void addElement(T value) {
         if (currentSize == array.length) resize();
         array[currentSize++] = value;
     }
+
     public void resize() {
         Object[] tempArray = new Object[array.length * 2];
         for (int i = 0; i < array.length; i++) {
@@ -25,6 +30,7 @@ public class CustomArrayImpl<T> implements CustomArray<T> {
         }
         array = tempArray;
     }
+
     @Override
     public void deleteElement(int index) {
         checkIndex(index);
@@ -32,23 +38,27 @@ public class CustomArrayImpl<T> implements CustomArray<T> {
         reBuild(index);
         currentSize--;
     }
+
     @Override
     public void updateElement(int index, T value) {
         checkIndex(index);
         array[index] = value;
     }
+
     public void reBuild(int index) {
         for (int i = index; i < currentSize; i++) {
             array[i] = array[i + 1];
             array[i + 1] = null;
         }
     }
+
     @Override
     public void print() {
         for (int i = 0; i < currentSize; i++) {
             System.out.print(array[i] + " ");
         }
     }
+
     private void checkIndex(int index) {
         if (index < 0 || index >= currentSize) {
             throw new IndexOutOfBoundsException(
